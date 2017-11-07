f078d24de41700d4b2a31cb210c45fb27b6f1f3e
w594533 <w594533@163.com>
2017-11-07 07:32:36 +0000

    新闻图片

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+  public function getCoverAttribute($cover)
+  {
+    return config('app.url'). '/storage/'. $cover;
+  }
+
 }
